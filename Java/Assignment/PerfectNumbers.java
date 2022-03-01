9b5b0c4a1bc8ef5e27becf524058578ba7e4f0b6
Bhavesh Mehta <bhavesh622@hotmail.com>
2022-03-01 20:32:56 +0530

    shifting files

---
+++ Java/Assignment/PerfectNumbers.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+public class PerfectNumbers {
+ boolean isPerfect(int aPosInt)
+ int generateFactors(int div){
+     int [] factors = new int[(int) Math.sqrt(div)];
+     for(int i = 1;i*i <= div; i++){
+        if(div % i == 0)
+            
+
+     }
+
+ }
+}
